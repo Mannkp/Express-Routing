7252c84f22638d0632326e5d3d892412bd13f554
Mannkumar_K._Pandya <75202120+Mannkp@users.noreply.github.com>
2023-08-17 00:58:06 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -24,6 +24,18 @@ app.get("/r/:subreddit/:category" , (req, res) => {
     res.status(200).send(`<h1 style='color: olive;font-family: Arial, Helvetica, sans-serif;'>You searched for: "${subreddit}/${category}"</h1>`);
 })
 
+// WORKING WITH QUERY STRINGS***
+
+app.get("/search", (req, res) => {
+    const {q} = req.query; //my request url is: http://localhost:7030/search?q=mann -> so we used 'q' as variable
+    if (!q){
+        res.send('<h1>NOTHING FOUND IF NOTHING SEARCHED</h1>');
+    }
+    console.log("query string: ", req.query);
+    res.send(`you searched for ${q}`);
+})
+
+
 app.use("*", (req, res)=>{
     console.log("May be link not found(ERROR)");
     res.status(404).send("<h1 style='color: red;font-family: Arial, Helvetica, sans-serif;'>SORRY! ERROR 404, We cannot find the link you searched for! </h1>");
